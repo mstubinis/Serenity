c36e04cac9f6b335abfc79ff36dff26af18a4e5f
Mike Tubinis <michaeltubinis@gmail.com>
2016-04-07 13:41:33 -0400

    .

--- Engine/Engine/Main.cpp
+++ Engine/Engine/Main.cpp
@@ -14,31 +14,31 @@ int main(){
     const unsigned int appWidth = 1024;
     const unsigned int appHeight = 768;
     srand((unsigned)time(0));
-	Engine::Detail::EngineClass::m_RenderingAPI = ENGINE_RENDERING_API_OPENGL;
-	//Engine::Detail::EngineClass::m_RenderingAPI = ENGINE_RENDERING_API_DIRECTX;
-	#ifdef _WIN32
-		if(GetConsoleWindow() == NULL){ 
-			AllocConsole();
-		}
-		freopen("CONIN$", "r", stdin);
-		freopen("CONOUT$", "w", stdout);
-		freopen("CONOUT$", "w", stderr);
-		#ifndef ENGINE_DEBUG
-			ShowWindow(GetConsoleWindow(), SW_HIDE);//hide console window if in release mode
-		#endif
-	#endif
+    Engine::Detail::EngineClass::m_RenderingAPI = ENGINE_RENDERING_API_OPENGL;
+    //Engine::Detail::EngineClass::m_RenderingAPI = ENGINE_RENDERING_API_DIRECTX;
+    #ifdef _WIN32
+        if(GetConsoleWindow() == NULL){ 
+            AllocConsole();
+        }
+        freopen("CONIN$", "r", stdin);
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOUT$", "w", stderr);
+        #ifndef ENGINE_DEBUG
+            ShowWindow(GetConsoleWindow(), SW_HIDE);//hide console window if in release mode
+        #endif
+    #endif
 
-	Engine::Detail::EngineClass::init(appName,appWidth,appHeight);
+    Engine::Detail::EngineClass::init(appName,appWidth,appHeight);
 
-	#ifdef _WIN32
-	if(Engine::Detail::EngineClass::m_RenderingAPI == ENGINE_RENDERING_API_DIRECTX){
+    #ifdef _WIN32
+    if(Engine::Detail::EngineClass::m_RenderingAPI == ENGINE_RENDERING_API_DIRECTX){
         Engine::Detail::EngineClass::runDirectX();
-	}
-	#endif
+    }
+    #endif
     if(Engine::Detail::EngineClass::m_RenderingAPI == ENGINE_RENDERING_API_OPENGL){
         Engine::Detail::EngineClass::run();
-		Engine::Detail::EngineClass::destruct();
-		FreeConsole();
-	}
-	return 0;
+        Engine::Detail::EngineClass::destruct();
+        FreeConsole();
+    }
+    return 0;
 }
