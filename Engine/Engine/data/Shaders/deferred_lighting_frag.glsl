9e51628622dcba871a7b8c358a2d743c746a2a4f
Michael Tubinis <michaeltubinis@gmail.com>
2017-07-24 08:05:17 -0400

    Update deferred_lighting_frag.glsl

--- Engine/Engine/data/Shaders/deferred_lighting_frag.glsl
+++ Engine/Engine/data/Shaders/deferred_lighting_frag.glsl
@@ -37,19 +37,20 @@ float BeckmannDist(float theta, float _alpha, float pi){
     float b = (1.0 - cos2a) / (cos2a * _alpha);
     return exp(-b) / (pi * _alpha * (cos2a * cos2a));
 }
-float SchlickFrensel(float f0, float theta){
-    return f0 + (1.0-f0) * pow( 1.0 - theta, 5.0);
+vec3 SchlickFrensel(float f0, float theta){
+    float ret = f0 + (1.0-f0) * pow( 1.0 - theta, 5.0);
+    return vec3(ret);
 }
-vec3 CookTorr(float frensel,float vdoth, float vdotn, float ldotn, float ndoth,float _alpha, float pi){
+vec3 CookTorr(vec3 frensel,float vdoth, float vdotn, float ldotn, float ndoth,float _alpha, float pi){
     float Beck = BeckmannDist(ndoth,_alpha,pi);
 
     float a = (2.0 * (ndoth) * (vdotn)) / (vdoth);
     float b = (2.0 * (ndoth) * (ldotn)) / (vdoth);
     float G = min(1.0,min(a,b));
 
-    float fin = (Beck * frensel * G) / (max((4.0 * vdotn * ldotn),0.0) + 0.0001);
+    vec3 fin = (vec3(Beck) * frensel * vec3(G)) / (max((vec3(4.0) * vec3(vdotn) * vec3(ldotn)),vec3(0.0)) + vec3(0.0001));
 
-    return vec3(fin);
+    return fin;
 }
 vec3 CalcLightInternal(vec3 LightDir,vec3 PxlWorldPos,vec3 PxlNormal,vec2 uv){
     float Glow = texture2D(gMiscMap,uv).r;
@@ -67,7 +68,7 @@ vec3 CalcLightInternal(vec3 LightDir,vec3 PxlWorldPos,vec3 PxlNormal,vec2 uv){
 
     float kPi = 3.1415926535898;
     float smoothness = materials[index].g; //note: this value is automatically clamped 0 to 1 with the phyiscally based models below
-	float F0 = materials[index].r;
+    float F0 = materials[index].r;
     float roughness = 1.0 - smoothness; //only valid for physical lighting models
     float alpha = roughness * roughness;
     
@@ -75,19 +76,19 @@ vec3 CalcLightInternal(vec3 LightDir,vec3 PxlWorldPos,vec3 PxlNormal,vec2 uv){
     vec3 Half = normalize(LightDir + ViewDir);
     float NdotL = max(dot(PxlNormal, LightDir), 0.0);
     float NdotH = max(dot(PxlNormal, Half), 0.0);
-    
+    float VdotN = max(0.0, dot(ViewDir,PxlNormal));
+    float VdotH = max(0.0, dot(ViewDir,Half));
     DiffuseColor = (NdotL * LightDataD.xyz) * LightDataA.y;
 
     if(materials[index].b == 0.0){ // this is blinn phong (non-physical)
-
         float conserv = (8.0 + smoothness ) / (8.0 * kPi);
         SpecularAngle = conserv * pow(NdotH, smoothness);
     }		
     else if(materials[index].b == 1.0){ //this is phong (non-physical)
-
         float conserv = (2.0 + smoothness ) / (2.0 * kPi);
         vec3 Reflect = reflect(-LightDir, PxlNormal);
-        SpecularAngle = conserv * pow(max(dot(ViewDir, Reflect), 0.0), smoothness);
+	float VdotR = max(0.0, dot(ViewDir,Reflect));
+        SpecularAngle = conserv * pow(VdotR, smoothness);
     }
     else if(materials[index].b == 2.0){ //this is GGX (physical)
         float LdotH = max(0.0, dot(LightDir,Half));
@@ -100,10 +101,8 @@ vec3 CalcLightInternal(vec3 LightDir,vec3 PxlWorldPos,vec3 PxlNormal,vec2 uv){
         SpecularAngle = max(0.0, (NdotL * D * Fresnel / (LdotH*LdotH*(1.0-k2)+k2)) );
     }
     else if(materials[index].b == 3.0){ //this is Cook-Torrance (physical)
-        float VdotH = max(0.0, dot(ViewDir,Half));
-        float VdotN = max(0.0, dot(ViewDir,PxlNormal));
-        float Frensel = SchlickFrensel(F0,VdotH);
-		vec3 res = CookTorr(Frensel,VdotH,VdotN,NdotL,NdotH,alpha,kPi);
+        vec3 Frensel = SchlickFrensel(F0,VdotH);
+        vec3 res = CookTorr(Frensel,VdotH,VdotN,NdotL,NdotH,alpha,kPi);
         SpecularAngle = res.x;
     }
     else if(materials[index].b == 4.0){ //this is gaussian (physical)
@@ -117,21 +116,19 @@ vec3 CalcLightInternal(vec3 LightDir,vec3 PxlWorldPos,vec3 PxlNormal,vec2 uv){
     else if(materials[index].b == 6.0){ //this is PBR (physical)
         /*
         vec3 f0 = mix(vec3(0.04), albedo, metallic);
-        float VdotH = max(0.0, dot(ViewDir,Half));
-        float VdotN = max(0.0, dot(ViewDir,PxlNormal));  
         vec3 Lo = vec3(0.0);
-        float attenuation = 1.0;
-        float Distance = length(LightDir);
+        float attenuation = 1.0;  
         if(LightDataD.w != 0 && LightDataD.w != 2){
+	    float Distance = length(LightDir);
             attenuation = 1.0 / (max(1.0 , LightDataB.z + (LightDataB.w * Distance) + (LightDataC.x * Distance * Distance)));
         }
         
         vec3 radiance = LightDataD.xyz * attenuation; // calculate per-light radiance 
 
-        vec3 Frensel = vec3(SchlickFrensel(f0,vdoth));
+        vec3 Frensel = SchlickFrensel(f0,vdoth);
         vec3 kD = (vec3(1.0) - Frensel) * (1.0 - metallic);
 
-        vec3 n  =  CookTorr(Frensel.r,VdotH,VdotN,LdotN,NdotH,alpha,kPi);
+        vec3 n  = CookTorr(Frensel,VdotH,VdotN,LdotN,NdotH,alpha,kPi);
         float d = 4.0 * max(dot(PxlNormal, ViewDir), 0.0) * NdotL + 0.001; 
         
         Lo += (kD * albedo / vec3(kPi) + (n / vec3(d))) * radiance * NdotL; 
@@ -171,8 +168,8 @@ void main(void){
     vec3 PxlNormal = texture2D(gNormalMap, uv).rgb;
 
     vec3 lightCalculation = vec3(0.0);
-	vec3 LightPosition = vec3(LightDataC.yzw);
-	vec3 LightDirection = vec3(LightDataA.w,LightDataB.x,LightDataB.y);
+    vec3 LightPosition = vec3(LightDataC.yzw);
+    vec3 LightDirection = vec3(LightDataA.w,LightDataB.x,LightDataB.y);
 
     if(LightDataD.w == 0){
         lightCalculation = CalcLightInternal(normalize(LightPosition - PxlPosition),PxlPosition,PxlNormal,uv);
