0d36bdc22d37dcd63a9d430982eb7e045a418d86
Michael Tubinis <michaeltubinis@gmail.com>
2017-07-27 10:46:18 -0400

    Update GBuffer.h

--- Engine/Engine/GBuffer.h
+++ Engine/Engine/GBuffer.h
@@ -8,64 +8,67 @@
 #include <unordered_map>
 
 typedef unsigned int uint;
-
-//framebuffer pixel size = window size * this value here
-const float GBUFFER_DIVISIBLES[] = {1.0f, // (diffuse.rgb)
-                                    1.0f, // (normals.rgb)
-                                    1.0f, // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
-                                    1.0f, // (lighting.rgb)
-                                    0.5f, // bloom, & ssao as alpha
-                                    0.5f, // gods rays
-                                    0.5f, // free2 buffer
-                                    1.0f};// depth
-
-const int GBUFFER_TYPES[] =            {GL_RGB8,               // (diffuse.rgb)
-                                        GL_RGB16F,             // (normals.rgb)
-                                        GL_RGB8,               // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
-                                        GL_RGB16F,             // (lighting.rgb)
-                                        GL_RGBA8,              // bloom, & ssao as alpha
-                                        GL_RGB8,               // gods rays
-                                        GL_RGBA8,              // free2 buffer
-                                        GL_DEPTH_COMPONENT16}; // depth
-
-const int GBUFFER_PIXEL_TYPES[] =      {GL_RGB,                // (diffuse.rgb)
-                                        GL_RGB,                // (normals.rgb)
-                                        GL_RGB,                // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
-                                        GL_RGB,                // (lighting.rgb)
-                                        GL_RGBA,               // bloom, & ssao as alpha
-                                        GL_RGB,                // gods rays
-                                        GL_RGBA,               // free2 buffer
-                                        GL_DEPTH_COMPONENT};   // depth
-
-const int GBUFFER_FLOAT_TYPES[] =      {GL_UNSIGNED_BYTE,
-                                        GL_FLOAT,
-                                        GL_UNSIGNED_BYTE,
-                                        GL_FLOAT,
-                                        GL_UNSIGNED_BYTE,
-                                        GL_UNSIGNED_BYTE,
-                                        GL_UNSIGNED_BYTE,
-                                        GL_FLOAT};
-
-const int GBUFFER_ATTACHMENT_TYPES[] = {GL_COLOR_ATTACHMENT0,
-                                        GL_COLOR_ATTACHMENT1,
-                                        GL_COLOR_ATTACHMENT2,
-                                        GL_COLOR_ATTACHMENT3,
-                                        GL_COLOR_ATTACHMENT0,
-                                        GL_COLOR_ATTACHMENT4,
-                                        GL_COLOR_ATTACHMENT1,
-                                        GL_DEPTH_ATTACHMENT};
-
-enum BUFFER_TYPES {BUFFER_TYPE_DIFFUSE,
-                   BUFFER_TYPE_NORMAL,
-                   BUFFER_TYPE_MISC,
-                   BUFFER_TYPE_LIGHTING,
-                   BUFFER_TYPE_BLOOM,
-                   BUFFER_TYPE_GODSRAYS,
-                   BUFFER_TYPE_FREE2,
-                   BUFFER_TYPE_DEPTH,
-                   BUFFER_TYPE_NUMBER};
-
-
+const float GBUFFER_DIVISIBLES[] = {    //framebuffer pixel size = window size * this value here
+    1.0f, // (diffuse.rgb)
+    1.0f, // (normals.rgb)
+    1.0f, // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
+    1.0f, // (lighting.rgb)
+    0.5f, // bloom, & ssao as alpha
+    0.5f, // gods rays
+    0.5f, // free2 buffer
+    1.0f  // depth
+};
+const int GBUFFER_TYPES[] = {
+    GL_RGB8,               // (diffuse.rgb)
+    GL_RGB16F,             // (normals.rgb)
+    GL_RGB8,               // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
+    GL_RGB16F,             // (lighting.rgb)
+    GL_RGBA8,              // bloom, & ssao as alpha
+    GL_RGB8,               // gods rays
+    GL_RGBA8,              // free2 buffer
+    GL_DEPTH_COMPONENT16   // depth
+};
+const int GBUFFER_PIXEL_TYPES[] = {
+    GL_RGB,                // (diffuse.rgb)
+    GL_RGB,                // (normals.rgb)
+    GL_RGB,                // Glow & SpecularMap(Greyscale) & MaterialID THEN HDR
+    GL_RGB,                // (lighting.rgb)
+    GL_RGBA,               // bloom, & ssao as alpha
+    GL_RGB,                // gods rays
+    GL_RGBA,               // free2 buffer
+    GL_DEPTH_COMPONENT     // depth
+};
+const int GBUFFER_FLOAT_TYPES[] = {
+    GL_UNSIGNED_BYTE,
+    GL_FLOAT,
+    GL_UNSIGNED_BYTE,
+    GL_FLOAT,
+    GL_UNSIGNED_BYTE,
+    GL_UNSIGNED_BYTE,
+    GL_UNSIGNED_BYTE,
+    GL_FLOAT
+};
+const int GBUFFER_ATTACHMENT_TYPES[] = {
+    GL_COLOR_ATTACHMENT0,
+    GL_COLOR_ATTACHMENT1,
+    GL_COLOR_ATTACHMENT2,
+    GL_COLOR_ATTACHMENT3,
+    GL_COLOR_ATTACHMENT0,
+    GL_COLOR_ATTACHMENT4,
+    GL_COLOR_ATTACHMENT1,
+    GL_DEPTH_ATTACHMENT
+};
+enum BUFFER_TYPES {
+    BUFFER_TYPE_DIFFUSE,
+    BUFFER_TYPE_NORMAL,
+    BUFFER_TYPE_MISC,
+    BUFFER_TYPE_LIGHTING,
+    BUFFER_TYPE_BLOOM,
+    BUFFER_TYPE_GODSRAYS,
+    BUFFER_TYPE_FREE2,
+    BUFFER_TYPE_DEPTH,
+    BUFFER_TYPE_NUMBER
+};
 class TextureBuffer final: public Texture{
     private:
         class impl;
@@ -77,7 +80,6 @@ class TextureBuffer final: public Texture{
         const float divisor() const;
         const int attatchment() const;
 };
-
 class GBuffer final{
     private:
         class impl;
