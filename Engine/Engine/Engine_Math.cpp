23a964d6689000b27e94e0812b8ace3179fc83ba
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-01 08:19:37 -0400

    Update Engine_Math.cpp

--- Engine/Engine/Engine_Math.cpp
+++ Engine/Engine/Engine_Math.cpp
@@ -87,41 +87,13 @@ float Math::Max(float x, float y, float z){ return glm::max(x,glm::max(y,z)); }
 float Math::Max(float x, float y, float z, float w){ return glm::max(x,glm::max(y,glm::max(z,w))); }
 
 float Math::pack4FloatsInto1Float(float r,float g,float b,float a){
-	const float decode_x = 1.0f/255.0f;
-	const float decode_y = 1.0f/65025.0f;
-	const float decode_z = 1.0f/16581375.0f;
-	return glm::dot( glm::vec4(r,g,b,a), glm::vec4(1.0f, decode_x, decode_y, decode_z) );
+    float r = ((int)floor((a)*63) << 18) | ((int)floor((b)*63) <<  12) | ((int)floor((c)*63) <<   6) | ((int)floor((d)*63));
+    return r;
 }
 float Math::pack4FloatsInto1Float(glm::vec4& c){ return Math::pack4FloatsInto1Float(c.r,c.g,c.b,c.a); }
-
-float Math::pack3FloatsInto1(float x,float y,float z){
-    //Scale and bias
-    unsigned char _x = (unsigned char)(x*255.0f);    
-    unsigned char _y = (unsigned char)(y*255.0f);        
-    unsigned char _z = (unsigned char)(z*255.0f);       
-     
-    unsigned int packedColor = (_x << 16) | (_y << 8) | _z;
-    float packedFloat = (float) ( ((double)packedColor) / ((double) (1 << 24)) );  
-    return packedFloat;
+glm::vec4 Math::packFloatInto4Floats(float i){
+   return glm::mod(glm::vec4(i / 262144.0f, i / 4096.0f, i / 64.0f, i), 64.0f);
 }
-float Math::pack3FloatsInto1(glm::vec3& v){ return Math::pack3FloatsInto1(v.x,v.y,v.z); }
-glm::vec3 Math::unpackFloatInto3(float f){
-    float r = (float)fmod(f, 1.0f);
-    float g = (float)fmod(f, 1.0f);
-    float b = (float)fmod(f, 1.0f);
-
-    //Unpack to the -1..1 range
-    r = (r * 2.0f) - 1.0f;
-    g = (g * 2.0f) - 1.0f;
-    b = (b * 2.0f) - 1.0f;
-    
-    return glm::vec3(r,g,b);
-}
-float Math::pack3BytesInto1Float(unsigned char r,unsigned char g,unsigned char b){
-    return float(r) + (float(g) * 256.0f) + (float(b) * 256.0f * 256.0f);
-}
-
-
 
 void Math::translate(ObjectDynamic* obj,btVector3& vec,bool local){
     if(local){
