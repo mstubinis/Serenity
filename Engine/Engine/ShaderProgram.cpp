7fa740f7b56b21790e87600c8e01a244de0e3bce
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-02 11:40:26 -0400

    Update ShaderProgram.cpp

--- Engine/Engine/ShaderProgram.cpp
+++ Engine/Engine/ShaderProgram.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 using namespace Engine;
+using namespace std;
 
 #pragma region individualShaderFiles
 
@@ -16,22 +17,21 @@ class Shader::impl final{
     public:
         SHADER_TYPE m_Type;
         bool m_FromFile;
-        std::string m_Data;
-
-        void _construct(std::string& name, std::string& data, SHADER_TYPE type, bool fromFile,Shader* super){
+        string m_Data;
+        void _construct(string& name, string& data, SHADER_TYPE type, bool fromFile,Shader* super){
             m_Data = data;
             m_Type = type;
             m_FromFile = fromFile;
             super->setName(name);
         }
 };
-Shader::Shader(std::string name, std::string shaderFileOrData, SHADER_TYPE shaderType,bool fromFile):m_i(new impl){
+Shader::Shader(string name, string shaderFileOrData, SHADER_TYPE shaderType,bool fromFile):m_i(new impl){
     m_i->_construct(name,shaderFileOrData,shaderType,fromFile,this);
 }
 Shader::~Shader(){
 }
 SHADER_TYPE Shader::type(){ return m_i->m_Type; }
-std::string Shader::data(){ return m_i->m_Data; }
+string Shader::data(){ return m_i->m_Data; }
 bool Shader::fromFile(){ return m_i->m_FromFile; }
 
 #pragma endregion
@@ -49,6 +49,7 @@ struct DefaultShaderBindFunctor{void operator()(EngineResource* r) const {
 }};
 struct DefaultShaderUnbindFunctor{void operator()(EngineResource* r) const {
 }};
+struct srtKey{inline bool operator() ( Material* _1,  Material* _2){return (_1->name() < _2->name());}};
 
 class ShaderP::impl final{
     public:
@@ -57,11 +58,11 @@ class ShaderP::impl final{
 
         SHADER_PIPELINE_STAGE m_Stage;
         GLuint m_ShaderProgram;
-        std::vector<Material*> m_Materials;
-        std::unordered_map<std::string,GLint> m_UniformLocations;
+        vector<Material*> m_Materials;
+        unordered_map<string,GLint> m_UniformLocations;
         Shader* m_VertexShader;
         Shader* m_FragmentShader;
-        void _construct(std::string& name, Shader* vs, Shader* fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
+        void _construct(string& name, Shader* vs, Shader* fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
             m_Stage = stage;
             m_VertexShader = vs;
             m_FragmentShader = fs;
@@ -84,7 +85,7 @@ class ShaderP::impl final{
             super->setCustomBindFunctor(ShaderP::impl::DEFAULT_BIND_FUNCTOR);
             super->setCustomUnbindFunctor(ShaderP::impl::DEFAULT_UNBIND_FUNCTOR);
         }
-        void _construct(std::string& name, std::string& vs, std::string& fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
+        void _construct(string& name, string& vs, string& fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
             Shader* _vs = Resources::getShader(vs); Shader* _fs = Resources::getShader(fs);
             if(_vs == nullptr){
                 Resources::addShader(vs,vs,SHADER_TYPE_VERTEX,true);
@@ -96,7 +97,7 @@ class ShaderP::impl final{
             }
             _construct(name,_vs,_fs,stage,super);
         }
-        void _construct(std::string& name, Shader* vs, std::string& fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
+        void _construct(string& name, Shader* vs, string& fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
             Shader* _fs = Resources::getShader(fs);
             if(_fs == nullptr){
                 Resources::addShader(fs,fs,SHADER_TYPE_FRAGMENT,true);
@@ -104,7 +105,7 @@ class ShaderP::impl final{
             }
             _construct(name,vs,_fs,stage,super);
         }
-        void _construct(std::string& name, std::string& vs, Shader* fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
+        void _construct(string& name, string& vs, Shader* fs, SHADER_PIPELINE_STAGE stage,ShaderP* super){
             Shader* _vs = Resources::getShader(vs);
             if(_vs == nullptr){
                 Resources::addShader(vs,vs,SHADER_TYPE_VERTEX,true);
@@ -112,14 +113,10 @@ class ShaderP::impl final{
             }
             _construct(name,_vs,fs,stage,super);
         }
-
-
-
-
         void _destruct(){
             _cleanupRenderingContext();
         }
-        void _initRenderingContext(std::string& name){
+        void _initRenderingContext(string& name){
             m_ShaderProgram = _compileOGL(m_VertexShader,m_FragmentShader,name);
         }
         void _cleanupRenderingContext(){
@@ -127,24 +124,23 @@ class ShaderP::impl final{
             glDeleteProgram(m_ShaderProgram);
             m_UniformLocations.clear();
         }
-        GLuint _compileOGL(Shader* vs,Shader*  ps,std::string& _shaderProgramName){
+        GLuint _compileOGL(Shader* vs,Shader*  ps,string& _shaderProgramName){
             m_UniformLocations.clear();
-            // Create the shaders id's
             GLuint vid = glCreateShader(GL_VERTEX_SHADER);
             GLuint fid = glCreateShader(GL_FRAGMENT_SHADER);
  
-            std::string VertexShaderCode = ""; std::string FragmentShaderCode = "";
+            string VertexShaderCode = ""; string FragmentShaderCode = "";
 
             if(vs->fromFile()){
                 boost::iostreams::stream<boost::iostreams::mapped_file_source> str(vs->data());
-                for(std::string line; std::getline(str, line, '\n');){ VertexShaderCode += "\n" + line; }
+                for(string line; getline(str, line, '\n');){ VertexShaderCode += "\n" + line; }
             }
             else{
                 VertexShaderCode = vs->data();
             }
             if(ps->fromFile()){
                 boost::iostreams::stream<boost::iostreams::mapped_file_source> str1(ps->data());
-                for(std::string line; std::getline(str1, line, '\n');){ FragmentShaderCode += "\n" + line; }
+                for(string line; getline(str1, line, '\n');){ FragmentShaderCode += "\n" + line; }
             }
             else{
                 FragmentShaderCode = ps->data();
@@ -161,13 +157,13 @@ class ShaderP::impl final{
             // Check Vertex Shader
             glGetShaderiv(vid, GL_COMPILE_STATUS, &res);
             glGetShaderiv(vid, GL_INFO_LOG_LENGTH, &logLength);
-            std::vector<char> vError(logLength);
+            vector<char> vError(logLength);
             glGetShaderInfoLog(vid, logLength, NULL, &vError[0]);
 
             if(res == GL_FALSE) {
-                if(vs->fromFile()){ std::cout << "VertexShader Log (" + vs->data() + "): " << std::endl; }
-				else{               std::cout << "VertexShader Log (" + vs->name() + "): " << std::endl; }
-                std::cout << &vError[0] << std::endl;
+                if(vs->fromFile()){ cout << "VertexShader Log (" + vs->data() + "): " << endl; }
+		else{               cout << "VertexShader Log (" + vs->name() + "): " << endl; }
+                cout << &vError[0] << endl;
             }
  
             // Compile Fragment Shader
@@ -178,13 +174,13 @@ class ShaderP::impl final{
             // Check Fragment Shader
             glGetShaderiv(fid, GL_COMPILE_STATUS, &res);
             glGetShaderiv(fid, GL_INFO_LOG_LENGTH, &logLength);
-            std::vector<char> fError(logLength);
+            vector<char> fError(logLength);
             glGetShaderInfoLog(fid, logLength, NULL, &fError[0]);
  
             if(res == GL_FALSE) {
-                if(ps->fromFile()){ std::cout << "FragmentShader Log (" + ps->data() + "): " << std::endl; }
-                else{               std::cout << "FragmentShader Log (" + ps->name() + "): " << std::endl; }
-                std::cout << &fError[0] << std::endl;
+                if(ps->fromFile()){ cout << "FragmentShader Log (" + ps->data() + "): " << endl; }
+                else{               cout << "FragmentShader Log (" + ps->name() + "): " << endl; }
+                cout << &fError[0] << endl;
             }
 
             // Link the program id
@@ -196,12 +192,12 @@ class ShaderP::impl final{
             // Check the program
             glGetProgramiv(pid, GL_LINK_STATUS, &res);
             glGetProgramiv(pid, GL_INFO_LOG_LENGTH, &logLength);
-            std::vector<char> pError( std::max(logLength, int(1)) );
+            vector<char> pError( std::max(logLength, int(1)) );
             glGetProgramInfoLog(pid, logLength, NULL, &pError[0]);
  
             if(res == GL_FALSE) {
-                std::cout << "ShaderProgram Log : " << std::endl;
-                std::cout << &pError[0] << std::endl;
+                cout << "ShaderProgram Log : " << endl;
+                cout << &pError[0] << endl;
             }
             glDetachShader(pid,vid);
             glDetachShader(pid,fid);
@@ -222,7 +218,7 @@ class ShaderP::impl final{
                 for(_i = 0; _i < _count; ++_i){
                     glGetActiveUniform(pid, (GLuint)_i, _bufSize, &_length, &_size, &_type, _name);
                     if(_length > 0){
-                        std::string _name1((char*)_name, _length);
+                        string _name1((char*)_name, _length);
                         GLint _uniformLoc = glGetUniformLocation(pid,_name);
                         this->m_UniformLocations.emplace(_name1,_uniformLoc);
                     }
@@ -230,27 +226,22 @@ class ShaderP::impl final{
             }
             return pid;
         }
-        void _compileDX(std::string vs, std::string ps, bool fromFile){
-        
-        }
 };
 DefaultShaderBindFunctor ShaderP::impl::DEFAULT_BIND_FUNCTOR;
 DefaultShaderUnbindFunctor ShaderP::impl::DEFAULT_UNBIND_FUNCTOR;
 
-ShaderP::ShaderP(std::string& n, std::string& vs, std::string& fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
+ShaderP::ShaderP(string& n, string& vs, string& fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
     m_i->_construct(n,vs,fs,s,this);
 }
-ShaderP::ShaderP(std::string& n, Shader* vs, Shader* fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
+ShaderP::ShaderP(string& n, Shader* vs, Shader* fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
     m_i->_construct(n,vs,fs,s,this);
 }
-ShaderP::ShaderP(std::string& n, Shader* vs, std::string& fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
+ShaderP::ShaderP(string& n, Shader* vs, string& fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
     m_i->_construct(n,vs,fs,s,this);
 }
-ShaderP::ShaderP(std::string& n, std::string& vs, Shader* fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
+ShaderP::ShaderP(string& n, string& vs, Shader* fs, SHADER_PIPELINE_STAGE s):m_i(new impl){
     m_i->_construct(n,vs,fs,s,this);
 }
-
-
 ShaderP::~ShaderP(){
     m_i->_destruct();
 }
@@ -264,12 +255,8 @@ GLuint ShaderP::program(){ return m_i->m_ShaderProgram; }
 Shader* ShaderP::vertexShader(){ return m_i->m_VertexShader; }
 Shader* ShaderP::fragmentShader(){ return m_i->m_FragmentShader; }
 SHADER_PIPELINE_STAGE ShaderP::stage(){ return m_i->m_Stage; }
-std::vector<Material*>& ShaderP::getMaterials(){ return m_i->m_Materials; }
+vector<Material*>& ShaderP::getMaterials(){ return m_i->m_Materials; }
 
-
-struct less_than_key{
-    inline bool operator() ( Material* struct1,  Material* struct2){return (struct1->name() < struct2->name());}
-};
 void ShaderP::bind(){
     if(Engine::Renderer::Detail::RendererInfo::GeneralInfo::current_shader_program != this){
         GLuint p = this->program();
@@ -287,11 +274,11 @@ void ShaderP::unbind(){
 }
 void ShaderP::addMaterial(std::string materialName){
     if(materialName == "" || !Resources::Detail::ResourceManagement::m_Materials.count(materialName)){
-        std::cout << "Material : '" << materialName << "' does not exist (ShaderP::addMaterial()) Returning..." << std::endl;
+        cout << "Material : '" << materialName << "' does not exist (ShaderP::addMaterial()) Returning..." << endl;
         return;
     }
     Material* mat = Resources::getMaterial(materialName);
     m_i->m_Materials.push_back(mat);
-    std::sort(m_i->m_Materials.begin(),m_i->m_Materials.end(),less_than_key());
+    sort(m_i->m_Materials.begin(),m_i->m_Materials.end(),srtKey());
 }
-const std::unordered_map<std::string,GLint>& ShaderP::uniforms() const { return this->m_i->m_UniformLocations; }
+const unordered_map<string,GLint>& ShaderP::uniforms() const { return this->m_i->m_UniformLocations; }
