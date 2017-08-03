6a2ff893c4f3d4fdb6de65a6731b5ec5538e6e88
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-03 08:36:48 -0400

    Update MeshInstance.cpp

--- Engine/Engine/MeshInstance.cpp
+++ Engine/Engine/MeshInstance.cpp
@@ -225,10 +225,11 @@ void MeshInstance::setMaterial(string n){ m_i->_setMaterial(Resources::getMateri
 void MeshInstance::setMaterial(Material* m){ m_i->_setMaterial(m,this); }
 void MeshInstance::setOrientation(glm::quat& o){ m_i->m_Orientation = o; }
 void MeshInstance::setOrientation(float x,float y,float z){ 
-    float threshold = 0;
-    if(abs(x) > threshold) m_i->m_Orientation = (glm::angleAxis(-x, glm::vec3(1,0,0)));                        //pitch
-    if(abs(y) > threshold) m_i->m_Orientation = m_i->m_Orientation * (glm::angleAxis(-y, glm::vec3(0,1,0)));   //yaw
-    if(abs(z) > threshold) m_i->m_Orientation = m_i->m_Orientation * (glm::angleAxis(z,  glm::vec3(0,0,1)));   //roll
+    if(abs(x) < Object::m_RotationThreshold && abs(y) < Object::m_RotationThreshold && abs(z) < Object::m_RotationThreshold)
+        return;    
+    if(abs(x) > Object::m_RotationThreshold) m_i->m_Orientation = (glm::angleAxis(-x, glm::vec3(1,0,0)));                     //pitch
+    if(abs(y) > Object::m_RotationThreshold) m_i->m_Orientation = m_i->m_Orientation * (glm::angleAxis(-y, glm::vec3(0,1,0)));//yaw
+    if(abs(z) > Object::m_RotationThreshold) m_i->m_Orientation = m_i->m_Orientation * (glm::angleAxis(z,  glm::vec3(0,0,1)));//roll
     m_i->_updateModelMatrix();
 }
 Object* MeshInstance::parent(){ return m_i->m_Parent; }
