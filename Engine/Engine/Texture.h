6fb964d3b578f489c53679fb3ac6092122b596c7
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-14 14:12:28 -0400

    Update Texture.h

--- Engine/Engine/Texture.h
+++ Engine/Engine/Texture.h
@@ -39,11 +39,11 @@ class Texture: public EngineResource{
 
         uchar* pixels();
         GLuint& address();
-		GLuint& address(uint);
+        GLuint& address(uint);
         GLuint type();
         uint width();
         uint height();
-		uint numAddresses();
+        uint numAddresses();
         ushort mipmapLevels();
         bool mipmapped();
 
