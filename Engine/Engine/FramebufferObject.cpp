b45b54c15aff55b1b2e1f46920156cc26ab5d1ae
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-16 14:59:57 -0400

    Update FramebufferObject.cpp

--- Engine/Engine/FramebufferObject.cpp
+++ Engine/Engine/FramebufferObject.cpp
@@ -8,6 +8,58 @@
 using namespace Engine;
 using namespace std;
 
+unordered_map<uint,GLuint> _populateImageFormatMap(){
+    unordered_map<uint,GLuint> m;
+    
+    m.emplace(uint(Depth16), GL_DEPTH_COMPONENT16);
+    m.emplace(uint(Depth24), GL_DEPTH_COMPONENT24);
+    m.emplace(uint(Depth32), GL_DEPTH_COMPONENT32);
+    m.emplace(uint(Depth32F), GL_DEPTH_COMPONENT32F);
+    m.emplace(uint(Depth24Stencil8), GL_DEPTH24_STENCIL8);
+    m.emplace(uint(Depth32FStencil8), GL_DEPTH32F_STENCIL8);
+    m.emplace(uint(RGB10_A2), GL_RGB10_A2);
+    m.emplace(uint(RGB10_A2UI), GL_RGB10_A2UI);
+    m.emplace(uint(R11F_G11F_B10F), GL_R11F_G11F_B10F);
+    m.emplace(uint(SRGB8_ALPHA8), GL_SRGB8_ALPHA8);
+    m.emplace(uint(StencilIndex8), GL_STENCIL_INDEX8);
+    m.emplace(uint(SRGB8), GL_SRGB8);
+    m.emplace(uint(COMPRESSED_RGB_S3TC_DXT1_EXT), GL_COMPRESSED_RGB_S3TC_DXT1_EXT);
+    m.emplace(uint(COMPRESSED_RGBA_S3TC_DXT1_EXT), GL_COMPRESSED_RGBA_S3TC_DXT1_EXT);
+    m.emplace(uint(COMPRESSED_RGBA_S3TC_DXT3_EXT), GL_COMPRESSED_RGBA_S3TC_DXT3_EXT);
+    m.emplace(uint(COMPRESSED_RGBA_S3TC_DXT5_EXT), GL_COMPRESSED_RGBA_S3TC_DXT5_EXT);
+    m.emplace(uint(COMPRESSED_SRGB_S3TC_DXT1_EXT), GL_COMPRESSED_SRGB_S3TC_DXT1_EXT);
+    m.emplace(uint(COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT), GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT);
+    m.emplace(uint(COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT), GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT);
+    m.emplace(uint(COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT), GL_COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT);
+    m.emplace(uint(COMPRESSED_RED_RGTC1), GL_COMPRESSED_RED_RGTC1);
+    m.emplace(uint(COMPRESSED_SIGNED_RED_RGTC1), GL_COMPRESSED_SIGNED_RED_RGTC1);
+    m.emplace(uint(COMPRESSED_RG_RGTC2), GL_COMPRESSED_RG_RGTC2);
+    m.emplace(uint(COMPRESSED_SIGNED_RG_RGTC2), GL_COMPRESSED_SIGNED_RG_RGTC2);
+    m.emplace(uint(COMPRESSED_RGBA_BPTC_UNORM), GL_COMPRESSED_RGBA_BPTC_UNORM);
+    m.emplace(uint(COMPRESSED_SRGB_ALPHA_BPTC_UNORM), GL_COMPRESSED_SRGB_ALPHA_BPTC_UNORM);
+    m.emplace(uint(COMPRESSED_RGB_BPTC_SIGNED_FLOAT), GL_COMPRESSED_RGB_BPTC_SIGNED_FLOAT);
+    m.emplace(uint(COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT), GL_COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT);
+    m.emplace(uint(R3_G3_B2), GL_R3_G3_B2);
+    m.emplace(uint(RGB5_A1), GL_RGB5_A1);
+    m.emplace(uint(RGB10_A2), GL_RGB10_A2);
+    m.emplace(uint(RGB10_A2UI), GL_RGB10_A2UI);
+    m.emplace(uint(R11F_G11F_B10F), GL_R11F_G11F_B10F);
+    m.emplace(uint(RGB9_E5), GL_RGB9_E5);
+    m.emplace(uint(RGB565), GL_RGB565);
+    m.emplace(uint(RGB8), GL_RGB8);
+    m.emplace(uint(RGBA8), GL_RGBA8);
+    m.emplace(uint(RGB16), GL_RGB16);
+    m.emplace(uint(RGBA16), GL_RGBA16);
+    m.emplace(uint(RGB16F), GL_RGB16F);
+    m.emplace(uint(RGBA16F), GL_RGBA16F);
+    m.emplace(uint(RGB32F), GL_RGB32F);
+    m.emplace(uint(RGBA32F), GL_RGBA32F);
+    m.emplace(uint(R8), GL_R8);
+    m.emplace(uint(RG8), GL_RG8);
+    
+    return m;
+}
+
 unordered_map<uint,GLuint> _populateGLAttatchmentMap(){
     unordered_map<uint,GLuint> m;
 
@@ -27,7 +79,7 @@ unordered_map<uint,GLuint> _populateGLAttatchmentMap(){
     return m;
 }
 unordered_map<uint,GLuint> GL_ATTATCHMENT_MAP = _populateGLAttatchmentMap();
-
+unordered_map<uint,GLuint> GL_IMAGE_FORMAT_MAP = _populateImageFormatMap();
 
 struct FramebufferObjectDefaultBindFunctor{void operator()(BindableResource* r) const {
     Renderer::setViewport(0,0,m_FramebufferWidth,m_FramebufferHeight);
