faa5e079c485598b3ede71533fda46b7789ae01a
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-16 14:01:31 -0400

    Update FramebufferObject.cpp

--- Engine/Engine/FramebufferObject.cpp
+++ Engine/Engine/FramebufferObject.cpp
@@ -6,6 +6,7 @@
 
 using namespace Engine;
 
+
 struct FramebufferObjectDefaultBindFunctor{void operator()(BindableResource* r) const {
     Renderer::setViewport(0,0,m_FramebufferWidth,m_FramebufferHeight);
     Renderer::bindFBO(m_FBO);
@@ -21,6 +22,20 @@ struct FramebufferObjectDefaultUnbindFunctor{void operator()(BindableResource* r
     Renderer::setViewport(0,0,Resources::getWindowSize().x,Resources::getWindowSize().y);
 }};
 
+class RenderbufferObject::impl{
+    public:
+        GLuint m_RBO;
+        void _init(RenderbufferObject* super){
+            glGenRenderbuffers(1, &m_RBO);
+        }
+        void _destruct(RenderbufferObject* super){
+            glDeleteRenderbuffers(1, &m_RBO);
+        }
+};
+RenderbufferObject::RenderbufferObject(){
+    m_i->_init(this);
+}
+RenderbufferObject::~RenderbufferObject(){ m_i->_destruct(this); }
 
 class FramebufferObject::impl{
     public:
@@ -51,7 +66,6 @@ class FramebufferObject::impl{
             }
         }
         void _destruct(FramebufferObject* super){
-            glDeleteRenderbuffers(1, &m_RBO);
             glDeleteFramebuffers(1, &m_FBO);
         }
         void _resize(FramebufferObject* super,uint new_width,uint new_height){
@@ -61,10 +75,37 @@ class FramebufferObject::impl{
             m_FramebufferWidth = new_width; m_FramebufferHeight = new_height;
             glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, new_width, new_height);
             
-            Renderer::unbindFBO(m_FBO);
-            Renderer::unbindRBO(m_RBO);
+            Renderer::unbindFBO();
+            Renderer::unbindRBO();
         }
-
+        void _attatchTexture(FramebufferObject* super,Texture* texture,FramebufferAttatchment::Attatchment attatchment){
+            if(attatchment != FramebufferAttatchment::Depth && attatchment != FramebufferAttatchment::Stencil && attatchment != FramebufferAttatchment::DepthAndStencil){
+                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + attatchment,texture->type(),texture->address(), 0);
+            }
+            else{
+                if(attatchment == FramebufferAttatchment::Depth)
+                    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, texture->type(), texture->address(), 0);
+                else if(attatchment == FramebufferAttatchment::Stencil)
+                    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, texture->type(), texture->address(), 0);
+                else if(attatchment == FramebufferAttatchment::DepthAndStencil)
+                    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, texture->type(), texture->address(), 0);
+            }
+        }
+        void _attatchRenderbuffer(FramebufferObject* super,RenderbufferObject* rbo,FramebufferAttatchment::Attatchment attatchment){
+            Renderer::bindRBO(rbo);
+            if(attatchment != FramebufferAttatchment::Depth && attatchment != FramebufferAttatchment::Stencil && attatchment != FramebufferAttatchment::DepthAndStencil){
+                glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + attatchment,GL_RENDERBUFFER,rbo->address());
+            }
+            else{
+                if(attatchment == FramebufferAttatchment::Depth)
+                    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,GL_RENDERBUFFER,rbo->address());
+                else if(attatchment == FramebufferAttatchment::Stencil)
+                    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT,GL_RENDERBUFFER,rbo->address());
+                else if(attatchment == FramebufferAttatchment::DepthAndStencil)
+                    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,GL_RENDERBUFFER,rbo->address());
+            }
+            Renderer::unbindRBO();
+        }
 };
 FramebufferObjectDefaultBindFunctor FramebufferObject::impl::DEFAULT_BIND_FUNCTOR;
 FramebufferObjectDefaultUnbindFunctor FramebufferObject::impl::DEFAULT_UNBIND_FUNCTOR;
@@ -72,9 +113,7 @@ FramebufferObjectDefaultUnbindFunctor FramebufferObject::impl::DEFAULT_UNBIND_FU
 FramebufferObject::FramebufferObject(std::string name,uint w,uint h):BindableResource(name),m_i(new impl){
     m_i->_init(this,w,h);
 }
-FramebufferObject::~FramebufferObject(){
-    m_i->_destruct(this);
-}
-void FramebufferObject::resize(uint w,uint h){
-    m_i->_resize(this,w,h);
-}
+FramebufferObject::~FramebufferObject(){ m_i->_destruct(this); }
+void FramebufferObject::resize(uint w,uint h){ m_i->_resize(this,w,h); }
+void FramebufferObject::attatchTexture(Texture* t,FramebufferAttatchment::Attatchment a){ m_i->_attatchTexture(this,t,a); }
+void FramebufferObject::attatchRenderBuffer(Renderbuffer* t,FramebufferAttatchment::Attatchment a){ m_i->_attatchRenderbuffer(this,t,a); }
