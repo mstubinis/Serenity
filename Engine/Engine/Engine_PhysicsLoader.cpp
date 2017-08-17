95265fc3b831b2f16696e38aabefa6428fb364d3
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-17 13:52:16 -0400

    Delete Engine_PhysicsLoader.cpp

--- Engine/Engine/Engine_PhysicsLoader.cpp
+++
@@ -1,7 +0,0 @@
-#include "Engine_PhysicsLoader.h"
-#include "Collision.h"
-
-void Engine::Resources::Load::Physics::load(const Collision* collision){
-}
-void Engine::Resources::Load::Physics::unload(const Collision* collision){
-}
