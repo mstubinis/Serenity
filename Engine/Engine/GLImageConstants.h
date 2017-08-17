99c7cebeb27207e71855c8a6e20f7f749a3353f6
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-17 11:25:43 -0400

    Update GLImageConstants.h

--- Engine/Engine/GLImageConstants.h
+++ Engine/Engine/GLImageConstants.h
@@ -8,13 +8,24 @@
 
 typedef unsigned int uint;
 
+class ImagePixelType{
+    public: enum Type{
+        UNSIGNED_BYTE,BYTE,UNSIGNED_SHORT,SHORT,UNSIGNED_INT,INT,FLOAT,UNSIGNED_BYTE_3_3_2,
+        UNSIGNED_BYTE_2_3_3_REV,UNSIGNED_SHORT_5_6_5,UNSIGNED_SHORT_5_6_5_REV,UNSIGNED_SHORT_4_4_4_4,
+        UNSIGNED_SHORT_4_4_4_4_REV,UNSIGNED_SHORT_5_5_5_1,UNSIGNED_SHORT_1_5_5_5_REV,UNSIGNED_INT_8_8_8_8,
+        UNSIGNED_INT_8_8_8_8_REV,UNSIGNED_INT_10_10_10_2,UNSIGNED_INT_2_10_10_10_REV
+    };
+    public: static std::unordered_map<uint,GLuint> IMAGE_PIXEL_TYPE_MAP;
+            static GLuint at(ImagePixelType::Type t){ return IMAGE_PIXEL_TYPE_MAP.at(uint(t)); }
+};
+
 class ImagePixelFormat{
-	public: enum Format{
+    public: enum Format{
         RED,RG,RGB,BGR,RGBA,BGRA,RED_INTEGER,RG_INTEGER,RGB_INTEGER,BGR_INTEGER,RGBA_INTEGER,
         BGRA_INTEGER,STENCIL_INDEX,DEPTH_COMPONENT,DEPTH_STENCIL
     };
     public: static std::unordered_map<uint,GLuint> IMAGE_PIXEL_FORMAT_MAP;
-            static GLuint at(ImagePixelFormat::Format format){ return IMAGE_PIXEL_FORMAT_MAP.at(uint(format)); }
+            static GLuint at(ImagePixelFormat::Format f){ return IMAGE_PIXEL_FORMAT_MAP.at(uint(f)); }
 };
 
 class ImageInternalFormat{
@@ -27,7 +38,7 @@ class ImageInternalFormat{
         COMPRESSED_RGB_BPTC_UNSIGNED_FLOAT,R3_G3_B2,RGB5_A1,RGB9_E5,RGB565,RGB8,RGBA8,RGB16,RGBA16,RGB16F,RGBA16F,RGB32F,RGBA32F,R8,RG8
     };
     public: static std::unordered_map<uint,GLuint> IMAGE_INTERNAL_FORMAT_MAP;
-            static GLuint at(ImageInternalFormat::Format format){ return IMAGE_INTERNAL_FORMAT_MAP.at(uint(format)); }
+            static GLuint at(ImageInternalFormat::Format f){ return IMAGE_INTERNAL_FORMAT_MAP.at(uint(f)); }
 };
 class FramebufferAttatchment{
     public: enum Attatchment{
