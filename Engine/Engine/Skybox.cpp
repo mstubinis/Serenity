ac2cc864e56e28305648a6c5baade9b8f9f60cc2
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-18 08:47:15 -0400

    Update Skybox.cpp

--- Engine/Engine/Skybox.cpp
+++ Engine/Engine/Skybox.cpp
@@ -83,7 +83,7 @@ Skybox::Skybox(string name,Scene* scene):SkyboxEmpty(name,scene){
     string names[6] = {front,back,left,right,top,bottom};
 
     m_Texture = new Texture(names,name+"Cubemap",GL_TEXTURE_CUBE_MAP,true,ImageInternalFormat::SRGB8_ALPHA8);
-    m_Texture->genPBREnvMapData(32,m_Texture->width() / 4,512);
+    m_Texture->genPBREnvMapData(32,m_Texture->width() / 4);
 }
 Skybox::~Skybox(){
 }
