b2977797c4fa9bfe56bfc82997d642bf6e065812
Michael Tubinis <michaeltubinis@gmail.com>
2017-08-24 11:38:55 -0400

    Update Engine_ResourceBasic.cpp

--- Engine/Engine/Engine_ResourceBasic.cpp
+++ Engine/Engine/Engine_ResourceBasic.cpp
@@ -1,29 +1,31 @@
 #include "Engine_ResourceBasic.h"
 #include <iostream>
 
-EngineResource::EngineResource(std::string name){
+using namespace std;
+
+EngineResource::EngineResource(string name){
     m_Name = name;
     m_IsLoaded = false;
     m_UsageCount = 0;
 }
 EngineResource::~EngineResource(){
 }
-void EngineResource::setName(std::string name){ 
+void EngineResource::setName(string name){ 
     m_Name = name;
 }
-std::string& EngineResource::name(){
+string& EngineResource::name(){
     return m_Name;
 }
 void EngineResource::load(){
     if(!m_IsLoaded){
         m_IsLoaded = true;
-        std::cout << ": " << m_Name << " - loaded." << std::endl;
+        cout << ": " << m_Name << " - loaded." << endl;
     }
 }
 void EngineResource::unload(){
     if(m_IsLoaded && m_UsageCount == 0){
         m_IsLoaded = false;
-        std::cout << ": " << m_Name << " - unloaded." << std::endl;
+        cout << ": " << m_Name << " - unloaded." << endl;
     }
 }
 bool EngineResource::isLoaded(){ return m_IsLoaded; }
