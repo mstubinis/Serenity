89fc7a5deb438124391d1b21b2c4e9a76ddb6f89
mstubinis <michaeltubinis@gmail.com>
2021-03-09 15:22:26 -0500

    more implementations to systems

--- SerenityLib/include/serenity/ecs/systems/SystemBaseClass.h
+++ SerenityLib/include/serenity/ecs/systems/SystemBaseClass.h
@@ -2,11 +2,13 @@
 #ifndef ENGINE_ECS_SYSTEM_BASE_CLASS_H
 #define ENGINE_ECS_SYSTEM_BASE_CLASS_H
 
+class  Scene;
 namespace Engine::priv {
     class sparse_set_base;
     class ECS;
 }
 
+#include <cstdint>
 #include <vector>
 #include <tuple>
 #include <serenity/ecs/entity/Entity.h>
@@ -20,19 +22,19 @@ enum class SystemExecutionPolicy : uint32_t {
 };
 
 class SystemBaseClass {
-    using ComponentPoolContainer = std::vector<std::pair<uint32_t, Engine::priv::sparse_set_base*>>;
-    using FuncUpdate = void(*)(SystemBaseClass&, const float dt, Scene&);
-    using FuncComponentAddedToEntity = void(*)(SystemBaseClass&, void* component, Entity);
+    using ComponentPoolContainer         = std::vector<std::pair<uint32_t, Engine::priv::sparse_set_base*>>;
+    using FuncUpdate                     = void(*)(SystemBaseClass&, const float dt, Scene&);
+    using FuncComponentAddedToEntity     = void(*)(SystemBaseClass&, void* component, Entity);
     using FuncComponentRemovedFromEntity = void(*)(SystemBaseClass&, Entity);
-    using FuncEntityAddedToScene = void(*)(SystemBaseClass&, Scene&, Entity);
-    using FuncSceneEntered = void(*)(SystemBaseClass&, Scene&);
-    using FuncSceneLeft = void(*)(SystemBaseClass&, Scene&);
+    using FuncEntityAddedToScene         = void(*)(SystemBaseClass&, Scene&, Entity);
+    using FuncSceneEntered               = void(*)(SystemBaseClass&, Scene&);
+    using FuncSceneLeft                  = void(*)(SystemBaseClass&, Scene&);
     private:
         bool hasEntity(Entity entity) const noexcept;
         bool hasAssociatedComponent(uint32_t typeID) noexcept;
         void associateComponentImpl(uint32_t typeID);
         void sortEntities() noexcept;
-        void insertionSort(std::vector<Entity>& container);
+        void insertionSort(std::vector<Entity>& container) noexcept;
     protected:
         Engine::priv::ECS&             m_ECS;
         std::vector<Entity>            m_Entities;
@@ -56,8 +58,8 @@ class SystemBaseClass {
         void onSceneEntered(Scene& scene) noexcept;
         void onSceneLeft(Scene& scene) noexcept;
 
-        void addEntity(Entity entity);
-        void removeEntity(Entity entity);
+        void addEntity(Entity entity) noexcept;
+        void removeEntity(Entity entity) noexcept;
         void eraseEntity(std::vector<Entity>& vec, Entity entity);
 
         template<class T> void setUpdateFunction(T&& func) noexcept;
@@ -85,37 +87,47 @@ class SystemBaseClass {
 
 template<class SYSTEM, class ... COMPONENTS>
 class SystemCRTP : public SystemBaseClass {
+    public:
+        static inline uint32_t TYPE_ID = 0;
     private:
         template<class COMPONENT>
-        void buildTupleImpl(Entity entity, uint32_t& idx) {
-            COMPONENT* ret = getComponentPool(idx).getComponent<COMPONENT>(entity);
+        COMPONENT* buildTupleImpl(Entity entity, uint32_t& idx) {
+            COMPONENT* ret = getComponentPool<COMPONENT>(idx).getComponent(entity);
             ++idx;
             return ret;
         }
 
         template<class T>
-        std::tuple<> buildTuple(T args, Entity entity) {
+        std::tuple<T, Entity, COMPONENTS*...> buildTuple(T args, Entity entity) {
             uint32_t idx = 0;
-            return std::make_tuple(args, entity, buildTupleImpl<COMPONENTS>(entity, idx));
+            return std::make_tuple(args, entity, buildTupleImpl<COMPONENTS>(entity, idx)...);
+        }
+        template<class T>
+        std::tuple<T, COMPONENTS*...> buildTupleNoEntity(T args, Entity entity) {
+            uint32_t idx = 0;
+            return std::make_tuple(args, buildTupleImpl<COMPONENTS>(entity, idx)...);
         }
 
         template<class T, class F1, class F2>
         void forEachImpl(T args, F1 f1, F2 f2, SystemExecutionPolicy execPolicy) {
             if (m_Entities.size() < 100 || execPolicy == SystemExecutionPolicy::Normal) {
                 for (const auto entity : m_Entities) {
-                    auto tuple = (this->*f2(args, entity));
-                    std::apply(f1, std::move(tuple));
+                    std::apply(  f1, (this->*f2)(args, entity)  );
                 }
             }else{
-                //TODO: implement
-
+                auto pairs = Engine::splitVectorPairs(collection);
+                for (size_t k = 0U; k < pairs.size(); ++k) {
+                    Engine::priv::threading::addJob([&pairs, k, &f2, this]() {
+                        for (size_t j = pairs[k].first; j <= pairs[k].second; ++j) {
+                            std::apply(  f1, (this->*f2)(args, m_Entities[j])  );
+                        }
+                    });
+                }
                 if (execPolicy == SystemExecutionPolicy::ParallelWait) {
                     Engine::priv::threading::waitForAll();
                 }
             }
         }
-    public:
-        static inline uint32_t TYPE_ID = 0;
     public:
         SystemCRTP(Engine::priv::ECS& ecs)
             : SystemBaseClass{ ecs }
@@ -123,13 +135,13 @@ class SystemCRTP : public SystemBaseClass {
 
         template<class T>
         void forEach(void(*func)(T, Entity, COMPONENTS*...), T args, SystemExecutionPolicy execPolicy = SystemExecutionPolicy::Normal) {
-            using F = std::tuple<T, Entity, COMPONENTS*...>;
-            forEachImpl(args, func, &SystemCRTP::buildTuple, execPolicy);
+            using F = std::tuple<T, Entity, COMPONENTS*...>(SystemCRTP::*)(T, Entity);
+            forEachImpl<T, decltype(func), F>(args, func, &SystemCRTP::buildTuple, execPolicy);
         }
         template<class T>
         void forEach(void(*func)(T, COMPONENTS*...), T args, SystemExecutionPolicy execPolicy = SystemExecutionPolicy::Normal) {
-            using F = std::tuple<T, COMPONENTS*...>;
-            forEachImpl(args, func, &SystemCRTP::buildTupleNoEntity, execPolicy);
+            using F = std::tuple<T, COMPONENTS*...>(SystemCRTP::*)(T, Entity);
+            forEachImpl<T, decltype(func), F>(args, func, &SystemCRTP::buildTupleNoEntity, execPolicy);
         }
 };
 
